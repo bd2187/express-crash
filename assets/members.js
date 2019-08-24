67560c22361c15b6b6f2817a2050276c7a53d9c0
bd2187 <brandon.dionisio.26@gmail.com>
2019-08-24 16:03:46 -0700

    Initial commit. Create sample members data

---
+++ assets/members.js
@@ -0,0 +1,29 @@
+const members = [
+    {
+        id: 001,
+        email: "bob@mail.com",
+        active: true
+    },
+    {
+        id: 002,
+        email: "jill@mail.com",
+        active: false
+    },
+    {
+        id: 003,
+        email: "ben@mail.com",
+        active: true
+    },
+    {
+        id: 004,
+        email: "dog@mail.com",
+        active: false
+    },
+    {
+        id: 005,
+        email: "cat@mail.com",
+        active: true
+    }
+];
+
+module.exports = members;
